c75ec31a9faa17679a054fac929f8932c2cbf472
Kainat Sikandar <kainatawaan33@gmail.com>
2026-02-23 15:48:24 +0500

    these are function

---
+++ datatype.py
@@ -0,0 +1,33 @@
+
+Kainat Sikandar <kainatawaan33@gmail.com>
+3:32 PM (9 minutes ago)
+to kainatsikandar365
+
+#function
+23,45
+51,87
+24,455
+def sum(val1,val2):
+    return val1 + val2
+
+value1 = 23
+value2 = 45
+sum1 = value1 + value2
+print("sum1 ans", sum1)
+#adding sum1 values by using function sum
+print("sum1 values by using function sum", sum(23,45))
+
+value3 = 51
+value4 = 87
+sum2 = value3 + value4
+print("sum2 ans", sum2)
+#adding sum2 values by using function sum
+print("sum2 values by using function sum", sum(value3,value4))
+      
+
+value5 = 24
+value6 = 455
+sum3 = value5 + value6
+print("sum3 ans", sum3)
+#adding sum3 values by using function sum
+print("sum3 values by using function sum", sum(24,455))
