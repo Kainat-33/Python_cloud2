178ebd82b9c8a65db9a0184c7b44405eb9e2b9a0
Kainat Sikandar <kainatawaan33@gmail.com>
2026-02-23 15:01:53 +0500

    these are the basuc veriables of pyhton langague

---
+++ Basic.py
@@ -0,0 +1,12 @@
+## data types
+# Numaric
+# integar: 23422, -3, 0, -234
+# Float: 234.233, -3.0, -1.0
+# text
+# string: "we are learning datatypes", 'abc garden town', '''text''', """text"""
+# boolen: true,flase
+## collection
+list = cloud_class_students = ["ali", "hamza","abc","aaa"]
+tuple = ("ali", "hamza", "abc", "aaa")
+Dictionary = student_data = {'name':"ali", 'age':17, "address":"abc street multan"}
+Set:{12,14,81}
